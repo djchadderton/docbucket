0dc9db5d3fe166fcc373ae1d52567f2172fea8b0
David Chadderton <david@chadderton.org>
2023-07-02 14:58:20 +0100

    Makes posts count default to 0 not nil

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_25_131446) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_02_135116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,7 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_25_131446) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "posts_count"
+    t.integer "posts_count", default: 1, null: false
   end
 
   create_table "posts", force: :cascade do |t|
