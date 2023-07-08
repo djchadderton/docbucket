9a1dc2653ccd333e5d2644bd3e9953b5e659c8ff
David Chadderton <david@chadderton.org>
2023-07-08 20:51:34 +0100

    Moves main page into a homepage controller

--- app/form_builders/rhino_form_builder.rb
+++ app/form_builders/rhino_form_builder.rb
@@ -1,8 +1,8 @@
 class RhinoFormBuilder < ActionView::Helpers::FormBuilder
-  delegate :tag, to: :@template
+  delegate :tag, :rails_service_blob_url, :rails_direct_uploads_url, to: :@template
 
   def rhino_editor(name, *args)
     hidden_field(name, id: field_id(name), value: object.send(name).try(:to_trix_html) || object.send(name)) +
-      tag.rhino_editor(input: field_id(name))
+      tag("rhino-editor", {input: field_id(name), data: {"blob-url-template": rails_service_blob_url(":signed_id", ":filename"), "direct-upload-url": rails_direct_uploads_url}}.merge(options))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 #
 
 Rails.application.routes.draw do
-  root "folders#index"
+  root "homepage#show"
   post "sessions/create"
   get "login", to: "sessions#new", as: "login"
   delete "logout", to: "sessions#destroy", as: "logout"
